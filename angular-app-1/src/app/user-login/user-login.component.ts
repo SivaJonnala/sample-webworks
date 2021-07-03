fbdad9adbcb64d1b32f707037eab474d8ea8d526
Teja Tummala <tummalatejaram@gmail.com>
2021-07-03 23:31:16 +0530

    variable names changes

--- angular-app-1/src/app/user-login/user-login.component.ts
+++ angular-app-1/src/app/user-login/user-login.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ['./user-login.component.scss']
 })
 export class UserLoginComponent implements OnInit {
+  public welcomeMessage: string = 'Hello Teja';
+  public firstName: string = 'First Name';
+  public phoneNumber: string = 'Number';
+  public mail : string = 'Email';
+  public message : string = 'Message';
 
   constructor() { }
 
